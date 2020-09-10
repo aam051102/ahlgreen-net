605f87529cc35295b39a9d72a97e74bc344ddc73
4480135 <4480135@RTS-365.dk>
2020-09-10 12:53:26 +0200

    SEO improvements and organization.

--- views/index/src/js/Home.js
+++ views/index/src/js/Home.js
@@ -32,14 +32,14 @@ const Home = () => {
                     name="Alexander Ahlgreen"
                     image_src={require("../assets/images/logo.png")}
                 >
-                    <h1>Alexander Ahlgreen</h1>
+                    <h1>Alexander Ahlgreen Madsen</h1>
                     <p>
                         I'm a Danish full-stack web developer with experience in
                         React, Node and anything in between. I mainly specialize
                         in web development, but I also enjoy developing C++
                         applications. I've been writing code since I was 10
-                        years old and I'm still going strong. Feel free to send
-                        challenges my way.
+                        years old and I'm still going strong. I love to be
+                        challenged, so feel free to send anything my way.
                     </p>
                     <p>
                         Expected to graduate as a web developer from Roskilde
