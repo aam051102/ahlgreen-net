d1d36155f58faee7503210efe34b2f295815b3a1
4480135 <4480135@RTS-365.dk>
2020-09-13 18:44:08 +0200

    Minor fixes.

--- views/index/src/js/PortfolioDetails.js
+++ views/index/src/js/PortfolioDetails.js
@@ -43,7 +43,6 @@ const PortfolioDetails = (props) => {
                     <h1>
                         <a
                             target="_blank"
-                            className="link"
                             href={
                                 creation.id
                                     ? decodeURIComponent(creation.url)
