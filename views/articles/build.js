cf26799408f3fcbc1378c4ca0ec8ecc434916cb1
MadCreativity <ahlgreenmadsen@gmail.com>
2022-12-27 16:48:06 +0100

    Update build.js.

--- views/articles/build.js
+++ views/articles/build.js
@@ -9,62 +9,68 @@ const templateContent = fs.readFileSync("./src/template/index.html").toString();
 const contentPaths = fs.readdirSync("./src/content");
 
 for (const p of contentPaths) {
-    const content = fs.readFileSync(path.join("./src/content", p)).toString();
+    if (path.extname(p) === ".md") {
+        const content = fs
+            .readFileSync(path.join("./src/content", p))
+            .toString();
 
-    const headMatch = content.match(/\{\{\s*HEAD\s*\}\}/);
-    const bodyMatch = content.match(/\{\{\s*BODY\s*\}\}/);
+        const headMatch = content.match(/\{\{\s*HEAD\s*\}\}/);
+        const bodyMatch = content.match(/\{\{\s*BODY\s*\}\}/);
 
-    // Header data
-    const headContent = content.substring(
-        headMatch.index + headMatch[0].length,
-        bodyMatch.index
-    );
+        // Header data
+        const headContent = content.substring(
+            headMatch.index + headMatch[0].length,
+            bodyMatch.index
+        );
 
-    const headLines = headContent
-        .split("\n")
-        .filter((l) => l.trim().length !== 0);
-    const head = headLines.reduce((prev, curr) => {
-        const def = curr.trim().split("=");
+        const headLines = headContent
+            .split("\n")
+            .filter((l) => l.trim().length !== 0);
+        const head = headLines.reduce((prev, curr) => {
+            const def = curr.trim().split("=");
 
-        return { ...prev, [def[0]]: def[1] };
-    }, {});
+            return { ...prev, [def[0]]: def[1] };
+        }, {});
 
-    // Content body
-    const markdownContent = content.substring(
-        bodyMatch.index + bodyMatch[0].length
-    );
+        // Content body
+        const markdownContent = content.substring(
+            bodyMatch.index + bodyMatch[0].length
+        );
 
-    const parsedContent = marked.marked(markdownContent, {
-        headerIds: true,
-        baseUrl: `/articles`,
-    });
+        const parsedContent = marked.marked(markdownContent, {
+            headerIds: true,
+            baseUrl: `/articles`,
+        });
 
-    let outputContent = templateContent;
+        let outputContent = templateContent;
 
-    outputContent = outputContent.replace(
-        /\{\{\s*title\s*\}\}/g,
-        `${head["title"]} | Article by MadCreativity`
-    );
+        outputContent = outputContent.replace(
+            /\{\{\s*title\s*\}\}/g,
+            `${head["title"]} | Article by MadCreativity`
+        );
 
-    outputContent = outputContent.replace(
-        /\{\{\s*description\s*\}\}/g,
-        head["description"] || ""
-    );
+        outputContent = outputContent.replace(
+            /\{\{\s*description\s*\}\}/g,
+            head["description"] || ""
+        );
 
-    outputContent = outputContent.replace(
-        /\{\{\s*path\s*\}\}/g,
-        "articles/" + p.substring(0, p.lastIndexOf("."))
-    );
+        outputContent = outputContent.replace(
+            /\{\{\s*path\s*\}\}/g,
+            "articles/" + p.substring(0, p.lastIndexOf("."))
+        );
 
-    outputContent = outputContent.replace(
-        /\{\{\s*content\s*\}\}/g,
-        parsedContent
-    );
+        outputContent = outputContent.replace(
+            /\{\{\s*content\s*\}\}/g,
+            parsedContent
+        );
 
-    fs.writeFileSync(
-        path.join("./build", p.substring(0, p.lastIndexOf(".")) + ".html"),
-        outputContent
-    );
+        fs.writeFileSync(
+            path.join("./build", p.substring(0, p.lastIndexOf(".")) + ".html"),
+            outputContent
+        );
+    } else {
+        fs.copyFileSync(p, path.resolve("./build/", p));
+    }
 }
 
 // Copy necessary files
